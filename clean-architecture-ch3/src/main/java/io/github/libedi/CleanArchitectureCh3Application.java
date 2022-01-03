02e018df3e12c620ffab57e133ce3242b239a943
Sangjun, Park <libedi@gmail.com>
2022-01-03 20:57:04 +0900

    3장 코드 구성하기 - 샘플코드 커밋

---
+++ clean-architecture-ch3/src/main/java/io/github/libedi/CleanArchitectureCh3Application.java
@@ -0,0 +1,13 @@
+package io.github.libedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureCh3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureCh3Application.class, args);
+	}
+
+}
---
+++ clean-architecture-ch3/src/test/java/io/github/libedi/CleanArchitectureCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.libedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureCh3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
